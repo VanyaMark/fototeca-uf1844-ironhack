91ae44d44ae803bf9bf22f9f7c432962bbe6787a
VanyaMark <markova.vanya@gmail.com>
2024-07-10 23:21:21 +0200

    delete functionality implemented

--- app.js
+++ app.js
@@ -13,8 +13,40 @@ app.use(express.urlencoded({ extended: true }));
 // Añadimos el middleware necesario para que el client puedo hacer peticiones GET a los recursos públicos de la carpeta 'public'
 app.use(express.static('public'));
 
-// Base de datos de imágenes
-const images = [];
+// Forma más simple. Variable global para saber cual es el siguiente Id que nos tocan
+let id = 5;
+//Base de datos
+let images = [
+    {
+      id: 1,
+      title: "happy cat",
+      imageUrl:
+        "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg",
+      datePic: "2024-07-3",
+    },
+    {
+      id: 2,
+      title: "happy dog",
+      imageUrl:
+        "https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      datePic: "2024-02-11",
+    },
+    {
+      id: 3,
+      title: "cat snow",
+      imageUrl:
+        "https://images.pexels.com/photos/3923387/pexels-photo-3923387.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      datePic: "2024-04-21",
+    },
+    {
+      id: 4,
+      title: "camera",
+      imageUrl:
+        "https://images.pexels.com/photos/19607905/pexels-photo-19607905/free-photo-of-photos-and-insta-camera-on-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      datePic: "2024-06-24",
+    },
+  ];
+
 
 // Especificar a Express que quiero usar EJS como motor de plantillas
 app.set('view engine', 'ejs');
@@ -34,7 +66,7 @@ app.get('/search', (req, res) => {
     // Extract query string
     const {keyword } = req.query;
 
-    const searchResult = images.filter(i => i.title.includes(keyword.toUpperCase()));
+    const searchResult = images.filter(i => i.title.toUpperCase().includes(keyword.toUpperCase()));
 
     res.render('home', {
         images: searchResult
@@ -50,7 +82,7 @@ app.get('/add-image-form', (req, res) => {
 
     res.render('image-form',  {
         isImageAdded: undefined,
-        imageAlreadyAdded: undefined,
+        isImageAlreadyAdded: undefined,
         duplicatedImageUrl: undefined, 
         today
     })
@@ -67,22 +99,24 @@ app.post('/add-image-form', (req, res) => {
     // 1. Actualizar el array 'images' con la información de req.body
     const { title, imageUrl, datePic } = req.body;
 
+      // Incremento la varible id para el obtener el siguiente identificador único
+      id++;
+
     //Validación del lado servidor del title
     if (!title || title.length > 30) {
         return res.status(400).send('Algo ha salido mal');
     }
 
-    let imageAlreadyAdded = false;
+    //let imageAlreadyAdded = false;
     let duplicatedImageUrl = undefined;
     const isImageAlreadyAdded = images.some(i => i.imageUrl === imageUrl)
     if (isImageAlreadyAdded) {
-        imageAlreadyAdded = true;
         duplicatedImageUrl = imageUrl;
-        console.log('imageAlreadyAdded: ', imageAlreadyAdded);
-        console.log('array when imageAdded true: ', images)
+
     } else {
             images.push({
-                title: title.toUpperCase(),
+                id,
+                title,
                 imageUrl,
                 datePic
             });
@@ -91,16 +125,31 @@ app.post('/add-image-form', (req, res) => {
             console.log('array sorted by date',images)
     }
 
-    // 3. Añadir los otros campos del formulario y sus validaciones 
-        // 4julio: Tras insertar una imagen 'dejaremos' el formulario visible 
-    //res.send('Datos recibidos');
+
     // Redirect es un método del objecto Response que permite 'redirigir' al cliente a un nuevo endpoint o vista
     res.render('image-form', {
         isImageAdded: true,
-        imageAlreadyAdded,
+        isImageAlreadyAdded,
         duplicatedImageUrl, 
         today
     });
+
+});
+
+// endpoint para borrar la imagen
+app.post('/images/:id/delete', (req, res) => {
+    // 1. ¿Cómo vamos a obtener la url de la imagen que quiere borrar el cliente? req.params.url
+    console.log('req params: ', req.params);
+    const { id } = req.params;
+    // 2. images? Usar el método filter para eliminar la imagen que me pasan por req.params.id
+
+    // Opción 1: 3. Sobreescribir el array images con el resultado del método filter
+    images = images.filter(i => i.id != id); 
+
+    // Opctión 2: Usar el método de array splice para eliminar el elemento del array images. Antes teneis que identificar el índice donde se encuentra la imagen que queremos borrar
+
+    // 3. Volvemos a hacer un render
+    res.redirect('/')
 
 });
 
